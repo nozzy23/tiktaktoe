d11ff1176b76f21ed534a5180c0a56826e05f9ce
Oscar Nunez <noscar07@gmail.com>
2021-03-31 02:04:33 -0400

    win check, choosing player and checking the board

--- code.py
+++ code.py
@@ -34,3 +34,39 @@ def place_marker(board,marker,position):
 
 place_marker(test_board,'$',8)
 display_board(test_board)
+
+def win_check(board, mark):
+    return ((board[1] == mark and board[2] == mark and board[3] == mark ) or #top row
+    (board[4] == mark and board[5] == mark and board[6] == mark) or #middle row
+    (board[7] == mark and board[8] == mark and board[9] == mark) or #bottom row
+    (board[1] == mark and board[4] == mark and board[7] == mark) or #left column
+    (board[2] == mark and board[5] == mark and board[8] == mark) or #middle column
+    (board[3] == mark and board[6] == mark and board[9] == mark) or #right column
+    (board[1] == mark and board[5] == mark and board[9] == mark) or #diagonal
+    (board[3] == mark and board[5] == mark and board[7] == mark)) #diagonal 
+
+win_check(test_board,'X')
+
+
+import random
+
+def choose_first():
+
+    if random.randint(0,1) == 0:
+        return 'Player 2'
+    else:
+        return 'Player 1'
+
+def space_check():
+
+    return board[position] == ' '
+
+
+def full_board_check(board):
+
+    for i in range(1-10):
+        if space_check(board,i):
+            return False
+    #board is full
+    return True 
+
