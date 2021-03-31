9dc4d4f15623e8e5dbd2fa93faf4ad14a1801245
Oscar Nunez <noscar07@gmail.com>
2021-03-31 01:35:48 -0400

    had player choose marker and set up function to set up markers position

--- code.py
+++ code.py
@@ -8,5 +8,29 @@ def display_board(board):
     print('- - -')
     print(board[7]+ '|' + board[8] + '|'+board[9])
 
-test_board = ['#','X','O','X','O','X','O','X','O','X']
+test_board = ['#','1','2','3','4','5','6','7','8','9']
+display_board(test_board)
+
+
+def player_input():
+
+    marker = ''
+
+    #asking player 1 to choce  or O
+
+    while marker != 'X' and marker != 'O':
+        marker = input('Player One would you like to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X','O')
+    else:
+        return ('O','X')
+
+player1_marker, player2_marker = player_input()
+
+def place_marker(board,marker,position):
+    board[position] = marker
+
+
+place_marker(test_board,'$',8)
 display_board(test_board)
